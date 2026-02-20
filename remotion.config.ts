be049eb3f2c2a922acc8106f00951428e9adee4a
RomanLeshchishin <romamail.ru365@gmail.com>
2026-02-20 15:51:31 +0500

    render

--- remotion.config.ts
+++ remotion.config.ts
@@ -6,3 +6,7 @@ Config.setConcurrency(2);
 Config.setCodec("h264");
 Config.setPixelFormat("yuv420p");
 Config.setImageSequence(false);
+
+if (process.platform === "linux") {
+  Config.setChromiumOpenGlRenderer("angle");
+}
